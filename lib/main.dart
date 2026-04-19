3915757d7b60537d0fab854bc1e8a6ca0eace04f
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-04-19 19:15:46 +0100

    Adding simple toast button and base button

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: Scaffold(body: Center(child: ToastButton('Show Toast'))),
+    );
+  }
+}
+
+class MyToastContent extends StatelessWidget {
+  const MyToastContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello! I am a simple toast! 🍞');
+  }
+}
+
+class MySpecialButton extends StatelessWidget {
+  const MySpecialButton(this.label, this.onPressed, {super.key});
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Text(label));
+  }
+}
+
+class ToastButton extends StatelessWidget {
+  const ToastButton(this.label, {super.key});
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    // In Flutter, instead of using classic class inheritance (extends MySpecialButton),
+    // we use "Composition". We simply return our original custom widget inside our build method!
+    // This allows us to reuse the design of MySpecialButton, while giving our new widget 
+    // a valid BuildContext to safely show the toast message!
+    return MySpecialButton(
+      label,
+      () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: MyToastContent()),
+        );
+      },
     );
   }
 }
