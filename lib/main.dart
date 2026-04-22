6a43aa54fe4d0233b2cc1797fe70b1d54e197200
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-04-22 17:16:19 +0100

    Adding theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,28 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Screen2Green',
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: myAppTheme,
       home: const HomeView(),
     );
   }
 }
+
+final ThemeData myAppTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xFFaac03e),
+    primary: const Color(0xFFaac03e),
+    secondary: const Color(0xFF637f30),
+    tertiary: const Color(0xFF5f4228),
+    surface: const Color(0xFFd9d8dd),
+  ),
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontFamily: 'LeagueSpartan',
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: TextStyle(fontFamily: 'LeagueSpartan'),
+    bodyLarge: TextStyle(fontFamily: 'UniverseCondensed'),
+    bodyMedium: TextStyle(fontFamily: 'UniverseCondensed'),
+  ),
+);
