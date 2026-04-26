a4f90dbd9eeb0876cedb39b39370915da2bbeba2
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-04-26 20:01:18 +0100

    Adding auth code and logout functionality to profile page

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final supabase = Supabase.instance.client;
+
+    return Center(
+      child: OutlinedButton(
+        onPressed: () async {
+          await supabase.auth.signOut();
+        },
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Colors.red,
+          backgroundColor: const Color.fromARGB(0, 239, 159, 159),
+        ),
+        child: const Text('Log out'),
+      ),
+    );
   }
 }
