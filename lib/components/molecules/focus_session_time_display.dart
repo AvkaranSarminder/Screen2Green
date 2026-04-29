d760d9538cbed60147fa2248a9c4ae6c8be35c8b
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-04-29 16:53:12 +0100

    Adding empty focus session time display widget

---
+++ lib/components/molecules/focus_session_time_display.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FocusSessionTimeDisplay extends StatelessWidget {
+  const FocusSessionTimeDisplay({
+    super.key,
+    required this.totalSeconds,
+    required this.remainingSeconds,
+  });
+
+  final int totalSeconds;
+  final int remainingSeconds;
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
