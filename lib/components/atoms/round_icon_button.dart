23d93c0deae35124285db484842118907eb44d1b
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-04-29 19:19:21 +0100

    Adding round icon button atom

---
+++ lib/components/atoms/round_icon_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class _RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const _RoundIconButton({required this.icon, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Material(
+      color: colorScheme.surfaceContainerHighest,
+      shape: const CircleBorder(),
+      child: InkWell(
+        onTap: onTap,
+        customBorder: const CircleBorder(),
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Icon(icon, color: colorScheme.onSurfaceVariant, size: 22),
+        ),
+      ),
+    );
+  }
+}
