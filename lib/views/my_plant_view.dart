226f8d3f761cba6130f8e2bbfe1269573259f7ae
Avkaran Sarminder <avkaran.sarminder.sd@gmail.com>
2026-05-06 14:54:06 +0100

    Turning plant view into stateful component

--- lib/views/my_plant_view.dart
+++ lib/views/my_plant_view.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MyPlantView extends StatelessWidget {
+class MyPlantView extends StatefulWidget {
   const MyPlantView({super.key});
 
+  @override
+  State<MyPlantView> createState() => _MyPlantViewState();
+}
+
+class _MyPlantViewState extends State<MyPlantView> {
   @override
   Widget build(BuildContext context) {
     return Container();
